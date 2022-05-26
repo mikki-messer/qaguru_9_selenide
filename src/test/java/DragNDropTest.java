2e545c36dcf30f6679d541de9b7181c04df062b8
Mike Bulanov <mikki.messer@gmail.com>
2022-05-26 19:47:12 +0300

    the drag and drop test implemented

---
+++ src/test/java/DragNDropTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.actions;
+
+public class DragNDropTest {
+    @BeforeAll
+    public static void setUp(){
+        Configuration.browserSize="1280x800";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @DisplayName("Тест функции drag-and-drop")
+    @Test
+    public void DragNDropActionsTest(){
+        Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(Condition.text("A"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(Condition.text("B"));
+    }
+}
--- src/test/java/GithubTest.java
+++ src/test/java/GithubTest.java
@@ -1,12 +1,11 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
@@ -17,20 +16,23 @@ public class GithubTest {
         Configuration.holdBrowserOpen = true;
     }
 
+    @DisplayName("Тест видимости статьи SoftAssertions в Selenide Github Wiki")
     @Test
     public void testGithub(){
         final String pageToFind = "SoftAssertions";
+        final String blockHeader = "Using JUnit5 extend test class";
+
         Selenide.open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $("#wiki-body").shouldHave(Condition.text("Welcome to the selenide wiki!"));
         $("#wiki-pages-filter").click();
         $("#wiki-pages-filter").setValue(pageToFind).pressEnter();
-        //TODO: переделать на проверку видимости элемента li
+
         $("[data-filterable-for=wiki-pages-filter]").shouldHave(Condition.text(pageToFind));
         $("[data-filterable-for=wiki-pages-filter]").$(By.linkText(pageToFind)).click();
         $(".gh-header").shouldHave(Condition.text(pageToFind));
 
-        $$("h4").findBy(Condition.text("Using JUnit5 extend test class")).sibling(0).shouldHave(Condition.cssClass("highlight"));
+        $$("h4").findBy(Condition.text(blockHeader)).sibling(0).shouldHave(Condition.cssClass("highlight"));
 
     }
 }
